ae0417ced90a40a622cec3f7ddfb13500385ecae
praptigawade12-sys <praptigawade12@gmail.com>
2026-06-15 11:47:34 +0400

    v1

---
+++ Factorial.py
@@ -0,0 +1,8 @@
+def fac(n):
+    if (n==0) or (n==1):
+        return 1
+    
+    return n*fac(n-1)
+
+n = int(input("Enter a number : "))
+print("Factorial of",n,"is",fac(n))
---
+++ Print1to10.py
@@ -0,0 +1,9 @@
+def print1to10(n):
+    if (n>10):
+        return
+    
+    print(n)
+    print1to10(n+1)
+
+n = int(input("Enter you number: "))
+print1to10(n)
